e69d10f635c16ccb9e76c24acb1e1578f8ccd596
OleksandrGrubyi <ghrubyi@ukr.net>
2022-06-29 22:48:35 +0300

    rm com

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { FormattedMessage } from "react-intl";
-import styles from "../Education/education.module.css";
-import EducationItem from "../EducationItem/EducationItem";
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import styles from '../Education/education.module.css'
+import EducationItem from '../EducationItem/EducationItem'
 
 function Education() {
   return (
@@ -10,16 +10,8 @@ function Education() {
         <FormattedMessage id="educationTitle" />
       </h3>
       <EducationItem />
-      {/* <section className={styles.mainInfo}>
-          <h2 className={styles.title}>
-            <FormattedMessage id="aboutTitle" />
-          </h2>
-          <span className={styles.text}>
-            <FormattedMessage id="aboutAdditional" />
-          </span>
-        </section> */}
     </div>
-  );
+  )
 }
 
-export default Education;
+export default Education
